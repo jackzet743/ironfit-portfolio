de717190fab1eb22197ffb5c10daa0e9e01f3885
jackzet743 <carlosmoya.pro2002@gmail.com>
2026-05-21 12:07:10 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -39,13 +39,13 @@ export default function IronfitLanding() {
                         Plataforma ecommerce de suplementación deportiva con pagos reales, automatización de emails y arquitectura escalable lista para producción.
                     </p>
 
-                    <div className="flex justify-center gap-4 pt-2">
+                    <div className="flex justify-center gap-4 pt-2 text-emerald-400">
                         <a 
                             href="https://github.com/jackzet743/ironfit-ecommerce-v2.git"
                             target="_blank"
                             rel="noopener noreferrer"
                         >
-                            <Button className="rounded-xl px-6 bg-slate-900 border border-slate-700 text-white hover:bg-slate-800 text-emerald-400">
+                            <Button className="rounded-xl px-6 bg-slate-900 border border-slate-700 text-white hover:bg-slate-800 ">
                                 Ver código
                             </Button>
                         </a>
@@ -132,13 +132,13 @@ export default function IronfitLanding() {
                     <h2 className="text-3xl font-semibold">Ready to explore?</h2>
                     <p className="text-slate-400">Proyecto listo para demostración técnica y revisión de código.</p>
 
-                    <div className="flex justify-center gap-4">
+                    <div className="flex justify-center gap-4 text-emerald-400">
                         <a 
                             href="https://github.com/jackzet743/ironfit-ecommerce-v2.git"
                             target="_blank"
                             rel="noopener noreferrer"
                         >
-                            <Button className="rounded-xl px-6 bg-slate-900 border border-slate-700 text-white hover:bg-slate-800 text-emerald-400">
+                            <Button className="rounded-xl px-6 bg-slate-900 border border-slate-700 text-white hover:bg-slate-800 ">
                                 Ver código
                             </Button>
                         </a>
